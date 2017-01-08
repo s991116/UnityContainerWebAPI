2dff780d588b8186ff6dfbb2943bbcf3bcbd0b4c
Philip Juhl <Philip.Juhl@gmail.com>
2017-01-08 13:24:13 +0100

    Added Test project with owin.testing

---
+++ WebApi/TestWebApiTest/TcWebApi.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiTest;
+
+namespace TestWebApiTest
+{
+    [TestClass]
+    public class TcWebApi
+    {
+        private static TestServer _server;
+
+        [ClassInitialize]
+        public static void FixtureInit(TestContext t)
+        {
+            _server = TestServer.Create<Startup>();
+        }
+
+        [ClassCleanup]
+        public static void FixtureDispose()
+        {
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public void WebApiGetTest()
+        {
+            //Arrange
+
+            //Act
+            var response = _server.HttpClient.GetAsync("/api/time").Result;
+
+            //Assert
+            var result = response.Content.ReadAsAsync<string>().Result;
+            Assert.AreEqual("hello world", result);
+        }
+    }
+}
